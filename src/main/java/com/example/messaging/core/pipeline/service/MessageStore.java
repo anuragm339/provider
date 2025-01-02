6a5a92b455239f8e533b4947a5e7a05b83fd244d
anurag339 <anuragm339@gmail.com>
2025-01-02 18:42:47 +0530

    use rocks db for compaction

--- src/main/java/com/example/messaging/core/pipeline/service/MessageStore.java
+++
@@ -1,9 +0,0 @@
-package com.example.messaging.core.pipeline.service;
-
-import com.example.messaging.models.Message;
-import java.util.Optional;
-import java.util.concurrent.CompletableFuture;
-
-public interface MessageStore {
-
-}
